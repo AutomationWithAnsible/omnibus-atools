aa1ce77673ea731ccc5856a50141753421971b36
Adham Helal <adham.helal@gmail.com>
2015-06-13 10:00:15 +0200

    todo

--- build.sh
+++ build.sh
@@ -34,12 +34,11 @@ $SUDO bundle install --binstubs
 
 old_hash="d7f7dd7e3ede3e323fc0e09381f16caf"
 new_hash="380df856e8f789c1af97d0da9a243769"
-if [ $(sudo find / -name cacert*.pem  -type f  -exec sed -i -e 's/d7f7dd7e3ede3e323fc0e09381f16caf/380df856e8f789c1af97d0da9a243769/g' {} \;) ]; then
-	echo ">  find . -name cacerts*.pem  -type f  -exec sed -i -e 's/d7f7dd7e3ede3e323fc0e09381f16caf/380df856e8f789c1af97d0da9a243769/g' {} \;"
-else
-	echo "> no pem issues"
-fi
 
+#TODO: FIX FOR MAC AND LINUX
+#echo ">  find / -name cacerts*.pem  -type f  -exec sed -i -e 's/d7f7dd7e3ede3e323fc0e09381f16caf/380df856e8f789c1af97d0da9a243769/g' {} \;"
+#sudo find / -name cacert.pem -type f  -exec sed -i -e 's/d7f7dd7e3ede3e323fc0e09381f16caf/380df856e8f789c1af97d0da9a243769/g' {} \;
+#sed -i.bak  -e 's/d7f7dd7e3ede3e323fc0e09381f16caf/380df856e8f789c1af97d0da9a243769/g' /var/cache/omnibus/cache/cacert.pem
 
 echo "Create prvoject dir in $PROJECT_DIR and cache dir in $PROJECT_CACHE_DIR"
 sudo mkdir -p $PROJECT_DIR
