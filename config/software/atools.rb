72c808a8d39be24bed587f81e29d27046eda0b67
Adham Helal <adham.helal@gmail.com>
2015-06-18 12:53:55 +0200

    added whichversion to support running multi version

--- config/software/atools.rb
+++ config/software/atools.rb
@@ -14,4 +14,14 @@ end
 
 
 build do
+	whichversion_path = "/usr/local/bin"
+	embedded_path = "/opt/atools/embedded/bin/"
+	#Deploy whichversion
+	erb source: "whichversion.erb",
+      dest: "#{whichversion_path}/whichversion",
+      mode: 0755,
+      vars: { whichversion_path: whichversion_path, embedded_path: embedded_path  }
+
+    # aeco path
+	command "ln -s #{whichversion_path}/whichversion #{whichversion_path}/aeco"
 end
