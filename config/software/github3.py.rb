514dfe7c8cd216f6d7b84c16581e132f236cc1b9
Adham Helal <adham.helal@gmail.com>
2015-06-19 17:44:55 +0200

    github3.py remove wrong installation option

--- config/software/github3.py.rb
+++ config/software/github3.py.rb
@@ -4,5 +4,5 @@ default_version "0.9.3"
 dependency "pip"
 
 build do
-  command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{default_version}"
+  command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} #{name}==#{default_version}"
 end
