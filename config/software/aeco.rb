4c31238d9fe813e78821df4e188a7c09072cb4aa
Adham Helal <adham.helal@gmail.com>
2015-06-19 16:17:57 +0200

    use https rather then git for aeco

--- config/software/aeco.rb
+++ config/software/aeco.rb
@@ -3,7 +3,7 @@ default_version "master"
 
 relative_path "aeco"
 
-source :git => "git@github.com:yetu/aeco.git"
+source :git => "https://github.com/yetu/aeco.git"
 
 build do
   command "#{install_dir}/embedded/bin/python setup.py install"
