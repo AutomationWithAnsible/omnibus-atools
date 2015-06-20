15a7d2a4fba2106a0af8c1162176eaa90fc01dd6
Adham Helal <adham.helal@gmail.com>
2015-06-20 02:23:28 +0200

    atools fixes

--- config/software/atools.rb
+++ config/software/atools.rb
@@ -13,15 +13,27 @@ build do
   whichversion_path = "/usr/local/bin"
   embedded_path = "/opt/atools/embedded/"
   embedded_bin_path = "#{embedded_path}/bin/"
-  
+  version_file = "#{embedded_bin_path}/whichversion"
   #Deploy whichversion
   erb source: "whichversion.erb",
-      dest: "#{embedded_bin_path}/whichversion",
+      dest: "#{version_file}",
       mode: 0755,
-      vars: { whichversion_path: whichversion_path, embedded_bin_path: embedded_bin_path  }
+      vars: { whichversion_path: whichversion_path, 
+              embedded_bin_path: embedded_bin_path,
+              version_file: version_file,
+            }
 
   erb source: "atools_version.erb",
       dest: "#{embedded_path}/version",
       mode: 0755,
       vars: { default_version: default_version }
+
+  erb source: "atools.py",
+      dest: "#{embedded_bin_path}/atools.py",
+      mode: 0755,
+      vars: { whichversion_path: whichversion_path, 
+              embedded_bin_path: embedded_bin_path,
+              version_file: version_file,
+            }
+
 end
