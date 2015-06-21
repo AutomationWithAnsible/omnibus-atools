7a704de9a95a22d9e200dd52023a32a57d34bca1
Adham Helal <adham.helal@gmail.com>
2015-06-21 12:15:12 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -e 
-su vagrant -c '
+echo "> WHOAMI=`whoami`"
+
+echo '
+#!/usr/bin/env bash
+set -e 
+echo "> WHOAMI=`whoami`"
+
 PROJECT="atools"
 PROJECT_USER="$USER"
 PROJECT_DIR="/opt/$PROJECT"
 PROJECT_CACHE_DIR="/var/cache/omnibus"
+
+source /home/vagrant/.bashrc
+
 echo "******** SETUP ubuntu ********"
 echo "PATH=$PATH"
 
@@ -14,16 +23,38 @@ ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
 echo "> apt-get update"
 sudo apt-get update
 
-echo "> apt-get install ruby git -y"
-sudo apt-get install ruby ruby-dev build-essential git -y
+echo "> apt-get install  git -y"
+sudo apt-get install build-essential git -y
 
-PROJECT="atools"
-echo "******** BUILD ubuntu ********"
-echo "> WHOAMI=`whoami`"
+
+cd
+if [ ! -d .rbenv ]; then 
+	git clone git://github.com/sstephenson/rbenv.git .rbenv
+	echo "export PATH=\"$HOME/.rbenv/bin:\$PATH\"" >> ~/.bashrc
+	export PATH="\$HOME/.rbenv/bin:$PATH"
+	echo "eval \"\$(rbenv init -)\"" >> ~/.bashrc
+fi
+
+if [ ! -d ~/.rbenv/plugins/ruby-build ]; then 
+	git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+	echo "export PATH=\"\$HOME/.rbenv/plugins/ruby-build/bin:\$PATH\"" >> ~/.bashrc
+fi
+
+if [ ! -d ~/.rbenv/plugins/rbenv-gem-rehash ]; then
+	git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+fi
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+if [ ! -d .rbenv ]; then 
+	rbenv install 2.2.2
+	rbenv global 2.2.2
+fi
+ruby -v
 
 
 echo "> PATH=$PATH"
-SUDO="sudo"
+SUDO=""
 
 if [ $(basename `pwd`) != "omnibus-$PROJECT" ]; then
 	echo "> cd omnibus-$PROJECT"
@@ -35,14 +66,6 @@ $SUDO gem install bundle --no-rdoc --no-ri
 echo "> $SUDO bundle install --binstubs"
 $SUDO bundle install --binstubs
 
-old_hash="d7f7dd7e3ede3e323fc0e09381f16caf"
-new_hash="380df856e8f789c1af97d0da9a243769"
-
-#TODO: FIX FOR MAC AND LINUX
-#echo ">  find / -name cacerts*.pem  -type f  -exec sed -i -e 's/d7f7dd7e3ede3e323fc0e09381f16caf/380df856e8f789c1af97d0da9a243769/g' {} \;"
-#sudo find / -name cacert.pem -type f  -exec sed -i -e 's/d7f7dd7e3ede3e323fc0e09381f16caf/380df856e8f789c1af97d0da9a243769/g' {} \;
-#sed -i.bak  -e 's/d7f7dd7e3ede3e323fc0e09381f16caf/380df856e8f789c1af97d0da9a243769/g' /var/cache/omnibus/cache/cacert.pem
-
 echo "Create prvoject dir in $PROJECT_DIR and cache dir in $PROJECT_CACHE_DIR"
 sudo mkdir -p $PROJECT_DIR
 sudo mkdir -p $PROJECT_CACHE_DIR
@@ -50,4 +73,6 @@ sudo chown $PROJECT_USER $PROJECT_CACHE_DIR
 sudo chown $PROJECT_USER $PROJECT_DIR
 echo "> omnibus build $PROJECT"
 $SUDO  /home/vagrant/omnibus-atools/bin/omnibus build $PROJECT 
-'
+' > /tmp/buid.sh
+chmod +x /tmp/buid.sh
+su - vagrant -c /tmp/buid.sh
